77098db65b22d65363e2cd1c639258af9e632e5a
vikas17a <vikas17a@gmail.com>
2014-05-14 16:46:49 +0530

    new scripts added and a sample log file

--- parse.pl
+++ parse.pl
@@ -4,15 +4,22 @@
 #$LOGFILE = "localhost_access_log.2014-05-13.txt";
 $LOGFILE = "access.log";
 open(LOGFILE) or die("Could not open log file.");
+$count = 0;
 foreach $line (<LOGFILE>) {
+    $count++;	
     ($site, $logName, $fullName, $date, $gmt,$req, $file, $proto, $status, $length) = split(' ',$line);
     $time = substr($date, 13);
     $date = substr($date, 1, 11);
     $req  = substr($req, 1);
     chop($gmt);
     chop($proto);
+    %data;
+    $data{$site} = 1;    
     # do line-by-line processing.
-	print "IP : $site --- Request : $req --- Status : $status --- Date : $date\n";
 }
+@keys = keys % data;
+$size = @keys;
+print "Unique Users : $size\n";
+print "Total Hits : $count\n";
 close(LOGFILE);
 
